3c2b95b86066a0ee7fd80b2afab309a1f227d629
Gokhan <gokankorkmaz@gmail.com>
2017-07-16 12:35:23 +0300

    Initial commit

---
+++ app/src/main/java/kodluyoruz/merhabakodluyoruz/SecondActivity.java
@@ -0,0 +1,14 @@
+package kodluyoruz.merhabakodluyoruz;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
+import static android.os.Build.VERSION_CODES.M;
+
+/**
+ * Created by GokhanKorkmaz on 8.07.2017.
+ */
+
+public class SecondActivity extends Activity {
+
+}
